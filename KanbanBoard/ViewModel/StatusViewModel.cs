90ca4370a2301e4030d81acce64c162e356d0469
ostapkuzyk <ostap.kuzyk@gmail.com>
2012-11-09 15:10:57 +0200

    Added Styles.xaml for views.

---
+++ KanbanBoard/ViewModel/StatusViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Practices.Prism.Commands;
+
+namespace KanbanBoard.ViewModel
+{
+    public class StatusViewModel : BaseViewModel
+    {
+        public DelegateCommand LogOutCommand { get; protected set; }
+
+        public StatusViewModel()
+        {
+                
+        }
+
+    }
+}
