593c27cccb08178733c7999a678cff27bb55793f
AndySm <asdev19@yahoo.com>
2012-11-15 16:08:30 +0200

    Added BoardItem entity.

---
+++ KanbanBoardDatabase/BoardItem.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[BoardItem]
+(
+	[Id] INT NOT NULL , 
+    [BoardId] UNIQUEIDENTIFIER NOT NULL, 
+    [Name] NVARCHAR(256) NOT NULL, 
+    CONSTRAINT [FK_BoardItem_Board] FOREIGN KEY ([BoardId]) REFERENCES [Board]([Id]), 
+    PRIMARY KEY ([BoardId])
+)
