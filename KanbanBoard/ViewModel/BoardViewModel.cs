a70e93d15baa1b2acf01c0090bbc532b94e348c7
ostapkuzyk <ostap.kuzyk@gmail.com>
2012-11-16 12:15:21 +0200

    Fixed Add new board command

--- KanbanBoard/ViewModel/BoardViewModel.cs
+++ KanbanBoard/ViewModel/BoardViewModel.cs
@@ -1,29 +1,38 @@
-﻿using Microsoft.Practices.Prism.Commands;
+﻿
+using KanbanBoard.Interfaces;
+using Microsoft.Practices.Prism.Commands;
 using Microsoft.Practices.Prism.Regions;
-
+using Microsoft.Practices.Unity;
+using System.ServiceModel.DomainServices.Client.ApplicationServices;
 namespace KanbanBoard.ViewModel
 {
+    using System;
+
     public class BoardViewModel : BaseViewModel, INavigationAware
-    {
+    {       
+        public static string  BoardIdParam = "BoardId";
+
+        private Guid BoardId;
+        
         public BoardViewModel()
             : base()
         {
+            
         }
 
         public bool IsNavigationTarget(NavigationContext navigationContext)
         {
-            return false;
-            //throw new System.NotImplementedException();
+            return true;
         }
 
         public void OnNavigatedFrom(NavigationContext navigationContext)
         {
-            //throw new System.NotImplementedException();
+            
         }
 
         public void OnNavigatedTo(NavigationContext navigationContext)
         {
-            //throw new System.NotImplementedException();
+            BoardId = Guid.Parse(navigationContext.Parameters[BoardIdParam]);
         }
     }
 }
