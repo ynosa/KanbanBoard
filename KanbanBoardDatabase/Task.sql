b34c4bdff0e8d54a382de308d1032c2f43937d88
AndySm <asdev19@yahoo.com>
2012-11-16 13:16:32 +0200

    Implemented BoardColumn, Task and Board relationship.

---
+++ KanbanBoardDatabase/Task.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Task]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [BoardColumnId] UNIQUEIDENTIFIER NOT NULL, 
+    [Name] NVARCHAR(256) NOT NULL, 
+    [Description] NVARCHAR(MAX) NOT NULL, 
+    [Position] SMALLINT NOT NULL, 
+    CONSTRAINT [FK_Task_ToBoardColumn] FOREIGN KEY ([BoardColumnId]) REFERENCES [BoardColumn]([Id])
+)
