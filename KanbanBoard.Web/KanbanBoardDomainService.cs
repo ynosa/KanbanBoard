268cf8ef76a9b1f1e5986abdb0856b20b6ced686
AndySm <asdev19@yahoo.com>
2012-11-22 11:13:10 +0200

    Implemented session expiration tracking.

--- KanbanBoard.Web/KanbanBoardDomainService.cs
+++ KanbanBoard.Web/KanbanBoardDomainService.cs
@@ -16,7 +16,7 @@ namespace KanbanBoard.Web
     // TODO: Add your application logic to these methods or in additional methods.
     // TODO: Wire up authentication (Windows/ASP.NET Forms) and uncomment the following to disable anonymous access
     // Also consider adding roles to restrict access as appropriate.
-    // [RequiresAuthentication]
+    [RequiresAuthentication]
     [EnableClientAccess()]
     public class KanbanBoardDomainService : LinqToEntitiesDomainService<KanbanBoardDatabaseEntities>
     {
