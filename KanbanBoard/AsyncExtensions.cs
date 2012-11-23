b1f4cdb06d41f49a7ea7f557bf0dea256aac15c1
Yaroslav Nosa <yaroslav.nosa@gmail.com>
2012-11-23 09:43:54 +0200

    1. Added Async support

---
+++ KanbanBoard/AsyncExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace KanbanBoard
+{
+    using System.ServiceModel.DomainServices.Client;
+    using System.Threading.Tasks;
+
+    public static class AsyncExtensions
+    {
+        public static Task<SubmitOperation> SubmitChangesAsync(this DomainContext context)
+        {
+            var t = new TaskCompletionSource<SubmitOperation>();            
+            context.SubmitChanges(obj => t.TrySetResult(obj), null);            
+            return t.Task;
+        }
+    }
+}
