2ee8c11d6a6fc53c34d231595ec26838bfebcf60
ostapkuzyk <ostap.kuzyk@gmail.com>
2012-11-23 11:19:05 +0200

    Fixxd build

---
+++ KanbanBoard.Shell/RegionAdapters/GridRegionAdapter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Specialized;
+using System.Linq;
+using Microsoft.Practices.Prism.Regions;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace KanbanBoard.Shell.RegionAdapters
+{
+    public class GridRegionAdapter : RegionAdapterBase<Grid>
+    {
+        public GridRegionAdapter(IRegionBehaviorFactory behaviorFactory) :
+            base(behaviorFactory)
+        {
+        }
+
+        protected override void Adapt(IRegion region, Grid regionTarget)
+        {
+            region.ActiveViews.CollectionChanged +=
+            (s, e) =>
+            {
+                if (e.Action == NotifyCollectionChangedAction.Add || e.Action == NotifyCollectionChangedAction.Replace)
+                {
+                    regionTarget.Children.Clear();
+                    regionTarget.Children.Add((UIElement)region.ActiveViews.First());
+                }
+            };
+            region.Views.CollectionChanged +=
+            (s, e) =>
+            {
+                if (e.Action == NotifyCollectionChangedAction.Add && region.ActiveViews.Count() == 0)
+                {
+                    region.Activate(e.NewItems[0]);
+                }
+            };
+        }
+        protected override IRegion CreateRegion()
+        {
+            return new SingleActiveRegion();
+        }
+
+    }
+}
